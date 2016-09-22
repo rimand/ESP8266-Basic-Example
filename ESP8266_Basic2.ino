c02f92289f4594a7336fc2570eef6eb375f22929
rimand <rimandstudio@gmail.com>
2016-09-22 22:23:40 +0700

    Add files via upload

---
+++ ESP8266_Basic2.ino
@@ -0,0 +1,47 @@
+#include <ESP8266WiFi.h>
+
+const char *ssid = "RimadStudio_Wifi";
+const char *pass = "b1321";
+
+WiFiClient client;
+
+IPAddress server_ip = {192,168,1,39};
+const int PORT = 8000;
+
+void setup() {
+  Serial.begin(112500);
+
+  WiFi.begin(ssid,pass);
+  while(WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(500);
+  } Serial.println();
+
+  Serial.print("Connected : ");
+  Serial.println(WiFi.localIP());
+
+  Serial.print("Open TCP Client");
+  while(!client.connect(server_ip,PORT)){
+    Serial.print(".");
+    delay(500);
+  } 
+  Serial.print(" -> ");
+  Serial.print(server_ip[0]); Serial.print(".");
+  Serial.print(server_ip[1]); Serial.print(".");
+  Serial.print(server_ip[2]); Serial.print(".");
+  Serial.println(server_ip[3]);
+}
+
+void loop() {
+  while(client.available()){
+    byte data = client.read();
+    Serial.write(data); 
+    if(client.available() == 0) Serial.println();
+  }
+  
+  while(Serial.available()){
+    byte data = Serial.read();
+    client.write(data);
+    if(Serial.available() == 0) client.println();
+  }
+}
